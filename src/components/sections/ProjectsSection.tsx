af8d6ad5404a4f8aad605f948d0c55cf6eec4a83
Prince Jha <imprincejha2004@gmail.com>
2025-07-05 15:13:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -67,7 +67,7 @@ const ProjectsSection = () => {
       y: 0,
       transition: { 
         duration: 0.7,
-        ease: [0.6, 0.05, -0.01, 0.9]
+        ease: [0.6, 0.05, 0.01, 0.9]
       },
     },
   };
